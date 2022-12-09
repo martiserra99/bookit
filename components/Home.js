86feaf47ba01abaacb21be1d3620d437db5eb59d
Marti Serra <martiserramolina99@gmail.com>
2022-12-09 11:12:55 +0100

    Setup serach and filter functionality

--- components/Home.js
+++ components/Home.js
@@ -7,16 +7,17 @@ import { clearErrors } from "../redux/actions/roomActions";
 
 import Pagination from "react-js-pagination";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const Home = () => {
   const router = useRouter();
 
   const dispatch = useDispatch();
-  const { rooms, resPerPage, roomsCount, error } = useSelector(
+  const { rooms, resPerPage, filteredRoomsCount, error } = useSelector(
     (state) => state.allRooms
   );
 
-  let { page = 1 } = router.query;
+  let { page = 1, location } = router.query;
   page = Number(page);
 
   useEffect(() => {
@@ -25,21 +26,26 @@ const Home = () => {
   }, [error, dispatch]);
 
   const handlePagination = (pageNumber) => {
-    router.push(`/?page=${pageNumber}`);
+    const queryParams = new URLSearchParams(window.location.search);
+    if (queryParams.has("page")) queryParams.set("page", pageNumber);
+    else queryParams.append("page", pageNumber);
+    router.replace({ search: queryParams.toString() });
   };
 
   return (
     <>
       <section id="rooms" className="container mt-5">
-        <h2 className="mb-3 ml-2 stays-heading">Stays in New York</h2>
+        <h2 className="mb-3 ml-2 stays-heading">
+          {location ? `Rooms in ${location}` : "All Rooms"}
+        </h2>
 
-        <a href="#" className="ml-2 back-to-search">
+        <Link href="/search" className="ml-2 back-to-search">
           {" "}
           <i className="fa fa-arrow-left"></i> Back to Search
-        </a>
+        </Link>
         <div className="row">
           {rooms && rooms.length === 0 ? (
-            <div className="alert alert-danger">
+            <div className="alert alert-danger mt-5 w-100">
               <b>No Rooms.</b>
             </div>
           ) : (
@@ -49,12 +55,12 @@ const Home = () => {
         </div>
       </section>
 
-      {resPerPage < roomsCount && (
+      {resPerPage < filteredRoomsCount && (
         <div className="d-flex justify-content-center mt-5">
           <Pagination
             activePage={page}
             itemsCountPerPage={resPerPage}
-            totalItemsCount={roomsCount}
+            totalItemsCount={filteredRoomsCount}
             onChange={handlePagination}
             nextPageText="Next"
             prevPageText="Prev"
--- redux/actions/roomActions.js
+++ redux/actions/roomActions.js
@@ -10,13 +10,17 @@ import {
 } from "../constants/roomConstants";
 
 export const getRooms =
-  (req, currentPage = 1) =>
+  (req, currentPage = 1, location = "", guests, category) =>
   async (dispatch) => {
     try {
       const { origin } = absoluteUrl(req);
-      const { data } = await axios.get(
-        `${origin}/api/rooms?page=${currentPage}`
-      );
+
+      let link = `${origin}/api/rooms?page=${currentPage}&location=${location}`;
+
+      if (guests) link += `&guestCapacity=${guests}`;
+      if (category) link += `&category=${category}`;
+
+      const { data } = await axios.get(link);
       dispatch({ type: ALL_ROOMS_SUCCESS, payload: data });
     } catch (error) {
       dispatch({ type: ALL_ROOMS_FAIL, payload: error.response.data.message });
